95282292fcb24acdc8331560e8c955d2c276dafd
m.zhaksygeldy <m.zhaksygeldy@p-s.kz>
2017-12-24 21:16:20 +0600

    add form

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -16,14 +16,33 @@ class Layout extends React.Component {
       const newId = contacts[contacts.length - 1].id + 1;
 
       this.setState({
-        contacts: contacts.concat({ id: newId, name: `New Contact ${newId}`, email: `${newId}@example.com` }),
+        contacts: contacts.concat({
+          id: newId,
+          name: this.refs.name.value,
+          email: this.refs.email.value,
+        }),
       });
+
+      this.refs.name.value = null;
+      this.refs.email.value = null;
     }
+
+    newContact = () =>
+        <div className='pure-g'>
+            <form className='pure-form' onSubmit={this.addContact}>
+                <fieldset>
+                    <legend>New Contact</legend>
+                    <input ref='email' type='email' placeholder='example@example.com'/>
+                    <input ref='name' type='text' placeholder='Name'/>
+                    <button type="submit" className="pure-button pure-button-primary">Add</button>
+                </fieldset>
+            </form>
+        </div>;
 
     render() {
       return (
             <div id='Layout'>
-                <a className="pure-button" href="#" onClick={this.addContact}>Add Contact</a>
+                {this.newContact()}
                 <div className='pure-g'>
                     {this.state.contacts.map(info =>
                         <Contact key={info.id} {...info}/>)}
